17c5892ea2341e51dd245ddbd178548bf2fc04b3
timetale1457 <75065237+timetale1457@users.noreply.github.com>
2023-10-31 16:11:01 +0900

    feat : 닉네임을 입력해서 join room을 할 수 있도록 함.

--- front/dalmuti/src/App.tsx
+++ front/dalmuti/src/App.tsx
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-let socket: Socket;
-
 const App = () => {
   const [roomId, setRoomId] = useState<string | null>(null);
+  const [inputRoomId, setInputRoomId] = useState('');
   const [nickname, setNickname] = useState('');
   const [message, setMessage] = useState('');
   const [chat, setChat] = useState<string[]>([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
-    if (!socket) return;
+    socket = io('http://localhost:4000');
+
+    socket.on('roomCreated', (roomId: string) => {
+      setRoomId(roomId);
+    });
+
+    socket.on('userJoined', (nickname: string) => {
+      setChat((prevChat) => [...prevChat, `${nickname} has joined`]);
+    });
+
+    socket.on('newMessage', (message: string) => {
+      setChat((prevChat) => [...prevChat, message]);
+    });
 
     socket.on('roomFull', () => {
       setError('The room is full.');
@@ -20,36 +31,57 @@ const App = () => {
     socket.on('roomNotExist', () => {
       setError('The room does not exist.');
     });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   const createRoom = () => {
-    socket = io('http://localhost:4000');
-    socket.emit('createRoom', { maxPeople: 10, roomName: 'My Room', nickname });
-    socket.on('roomCreated', (roomId: string) => {
-      setRoomId(roomId);
-    });
+    if (nickname) {
+      socket.emit('createRoom', { maxPeople: 10, roomName: 'My Room', nickname });
+    }
+  };
+
+  const joinRoomById = () => {
+    setRoomId(inputRoomId);
+    joinRoom();
   };
 
   const joinRoom = () => {
-    socket = io('http://localhost:4000');
-    socket.emit('joinRoom', roomId, nickname);
-    socket.on('userJoined', (nickname: string) => {
-      setChat([...chat, `${nickname} has joined`]);
-    });
+    if (roomId && nickname) {
+      socket.emit('joinRoom', roomId, nickname);
+    }
   };
 
   const sendMessage = () => {
-    socket.emit('sendMessage', roomId, message);
-    socket.on('newMessage', (message: string) => {
-      setChat([...chat, message]);
-    });
+    if (roomId && message) {
+      const fullMessage = `${nickname}: ${message}`;
+      socket.emit('sendMessage', roomId, fullMessage);
+      setMessage('');
+    }
   };
 
   return (
     <div>
       {error && <div>Error: {error}</div>}
       {!roomId ? (
-        <button onClick={createRoom}>Create Room</button>
+        <>
+          <input
+            type="text"
+            placeholder="Nickname"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          <button onClick={createRoom}>Create Room</button>
+          <input
+            type="text"
+            placeholder="Room ID"
+            value={inputRoomId}
+            onChange={(e) => setInputRoomId(e.target.value)}
+          />
+          <button onClick={joinRoomById}>Join Room by ID</button>
+        </>
       ) : (
         <>
           <h1>Room ID: {roomId}</h1>
@@ -71,4 +103,5 @@ const App = () => {
   );
 };
 
+let socket: Socket;
 export default App;
